07caa64c4a18c31d55e287e2d745ebe2f96bf409
Santiago Garcia Herrera <sngh966.sg@gmail.com>
2024-06-14 10:59:36 -0500

    Correción en la generación del la información del mapa de inversiones 2

--- app_mapabrechas-folium.py
+++ app_mapabrechas-folium.py
@@ -332,7 +332,7 @@ grouped_data2 = grouped_data[mask].groupby(['Municipio', 'Departamento'])[['IPM'
 grouped_data2 = grouped_data2.reset_index()
 grouped_data2['($) Valor Total'] = '$' + (round(grouped_data2['Valor Total'].astype(float)/1000000,2)).astype(str) + 'M.'
 grouped_data2['Municipio_Departamento'] = grouped_data2['Municipio'] + ', ' + grouped_data2['Departamento']
-#grouped_data2 = grouped_data2[grouped_data2['Valor Total'] > 0]
+grouped_data2 = grouped_data2[grouped_data2['Valor Total'] > 0]
 st.write(grouped_data2[grouped_data2['Valor Total'] == 0])
 st.write(grouped_data2['Valor Total'].isnull().sum())
 note = 'El gráfico presenta el valor total invertido en proyectos de carácter municipal, clasificados por departamento <br>Los cálculos no suman los proyectos de caracter Departamental, por lo que el precio total de Colombia no es el total.'
